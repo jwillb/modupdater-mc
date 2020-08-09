263a238883ae4c1e891e469792a9ebe04112b19e
BuildTools <jonathanwbonham@gmail.com>
2020-08-08 23:06:57 -0600

    Clean up code

--- modupdater.py
+++ modupdater.py
@@ -5,7 +5,6 @@ import wget
 import requests as rq
 import json
 
-
 # Import Selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,8 +12,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
-
-
+from selenium.common.exceptions import NoSuchElementException
 
 # Get current dir so script can be run from anywhere
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -42,25 +40,26 @@ options = Options()
 
 base_api_url = "https://curse.nikky.moe/api/addon/"
 
-print(current_dir)
+print("Current working directory: {}".format(current_dir))
 print("Using minecraft version {}".format(version))
 
-sleep(0.5)
-
-query_url = "https://google.com/search?q=site:curseforge.com+Aether2"
-
 # Get mod project ID number for the Curseproxy API
-browser.get(query_url)
-mod_link = browser.find_element_by_partial_link_text("Mods - Minecraft - CurseForge") 
-mod_link.click()
-files = browser.find_element_by_partial_link_text("Files")
-files.click
-current_browser_url = browser.current_url
-project_id = browser.find_element_by_css_selector("div.mb-3:nth-child(2) > div:nth-child(1) > span:nth-child(2)").text
+def get_project_id(mod_name):
+    query_url = "https://google.com/search?q=site:curseforge.com+" + mod_name
+    browser.get(query_url)
+    try:
+        mod_link = browser.find_element_by_partial_link_text("Mods - Minecraft - CurseForge") 
+        mod_link.click()
+    except NoSuchElementException:    
+            print("Element was not found. This could mean that there was an internal error, but \nit usually means that the mod is not on Curseforge.")
+            browser.quit()
+            exit()
+    files = browser.find_element_by_partial_link_text("Files")
+    files.click
+    project_id = browser.find_element_by_css_selector("div.mb-3:nth-child(2) > div:nth-child(1) > span:nth-child(2)").text
 
-browser.quit()
-print(project_id)
+    browser.quit()
+    return project_id
 
-api_data = json.loads(rq.get(base_api_url + project_id).content)
-
-print("Currently processing mod {}".format(api_data["name"]))
+mod_id = get_project_id("ProjectE-1.12.2-PE1.4.1.jar")
+print(mod_id)
