a3031e45c16d8548a67fba9134d18c3f5d56d366
BuildTools <jonathanwbonham@gmail.com>
2020-08-08 19:27:10 -0600

    Add custom geckopath option

--- modupdater.py
+++ modupdater.py
@@ -5,6 +5,7 @@ import wget
 import requests as rq
 import json
 
+
 # Import Selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,10 +14,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
-browser = webdriver.Firefox()
-options = Options()
 
-base_api_url = "https://curse.nikky.moe/api/addon/"
 
 # Get current dir so script can be run from anywhere
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -27,23 +25,34 @@ with open(current_dir + "/options.txt") as source:
     for line in source:
         options_txt.append(line)
 
+# Create geckopath variable and remove newline chars from options.txt
+geckopath = r""
+for character in options_txt[3]:
+    if character != "\n":
+        geckopath = geckopath + character
+
 # Create version variable and remove newline chars from options.txt
 version = ""
 for character in options_txt[1]:
     if character != "\n":
         version = version + character
 
+browser = webdriver.Firefox(executable_path=geckopath)
+options = Options()
+
+base_api_url = "https://curse.nikky.moe/api/addon/"
+
 print(current_dir)
 print("Using minecraft version {}".format(version))
 
 sleep(0.5)
 
-query_url = "https://google.com/search?q=site:curseforge.com+"
+query_url = "https://google.com/search?q=site:curseforge.com+Aether2"
 
 # Get mod project ID number for the Curseproxy API
 browser.get(query_url)
-links = browser.find_element_by_partial_link_text("Mods - Minecraft - CurseForge") 
-links.click()
+mod_link = browser.find_element_by_partial_link_text("Mods - Minecraft - CurseForge") 
+mod_link.click()
 files = browser.find_element_by_partial_link_text("Files")
 files.click
 current_browser_url = browser.current_url
